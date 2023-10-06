8f2452efd1225df915cf98f8364adb9236f70707
namanthanki <stuniz7855@gmail.com>
2023-10-06 23:08:11 +0530

    updated db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-const MongoDBAtlasURI = "mongodb+srv://namanthanki785:namanthanki@cluster0.pb0qvj1.mongodb.net/?retryWrites=true&w=majority";
 const MongoDBLocalURI = "mongodb://127.0.0.1:27017/greenbook"; 
 
 const connection = mongoose.createConnection(MongoDBLocalURI)
