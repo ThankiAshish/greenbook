f7290198218d899e2a330942582b35fff5402cd2
Ashish <thankiashish555@gmail.com>
2023-10-19 20:22:10 +0530

    Redesigned Profile Page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -49,222 +49,6 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ),
       ),
-      // body: SingleChildScrollView(
-      //   child: SizedBox(
-      //     width: MediaQuery.of(context).size.width,
-      //     child: Padding(
-      //       padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //         children: [
-      //           Row(
-      //             children: [
-      //               ClipOval(
-      //                 child: Image.asset(
-      //                   "backend/images/${user.profilePicture}",
-      //                   width: 100,
-      //                   height: 100,
-      //                   fit: BoxFit.cover,
-      //                 ),
-      //               ),
-      //               const SizedBox(
-      //                 width: 10,
-      //               ),
-      //               SizedBox(
-      //                 width: MediaQuery.of(context).size.width - 190,
-      //                 child: Column(
-      //                   crossAxisAlignment: CrossAxisAlignment.start,
-      //                   children: [
-      //                     Text(
-      //                       user.name,
-      //                       style: GoogleFonts.manrope(
-      //                         color: Colors.black,
-      //                         fontSize: 22,
-      //                         fontWeight: FontWeight.w500,
-      //                         height: 0,
-      //                         letterSpacing: 2.20,
-      //                       ),
-      //                     ),
-      //                     const SizedBox(height: 20),
-      //                     Row(
-      //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //                       children: [
-      //                         Column(
-      //                           children: [
-      //                             Text(
-      //                               '6.5K',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: Colors.black,
-      //                                 fontSize: 12,
-      //                                 fontWeight: FontWeight.w600,
-      //                                 height: 0,
-      //                                 letterSpacing: 1.20,
-      //                               ),
-      //                             ),
-      //                             Text(
-      //                               'Followers',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: const Color(0xFF20411B),
-      //                                 fontSize: 10,
-      //                                 fontWeight: FontWeight.w300,
-      //                                 height: 0,
-      //                                 letterSpacing: 1,
-      //                               ),
-      //                             ),
-      //                           ],
-      //                         ),
-      //                         Column(
-      //                           children: [
-      //                             Text(
-      //                               '386',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: Colors.black,
-      //                                 fontSize: 12,
-      //                                 fontWeight: FontWeight.w600,
-      //                                 height: 0,
-      //                                 letterSpacing: 1.20,
-      //                               ),
-      //                             ),
-      //                             Text(
-      //                               'Following',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: const Color(0xFF20411B),
-      //                                 fontSize: 10,
-      //                                 fontWeight: FontWeight.w300,
-      //                                 height: 0,
-      //                                 letterSpacing: 1,
-      //                               ),
-      //                             ),
-      //                           ],
-      //                         ),
-      //                         Column(
-      //                           children: [
-      //                             Text(
-      //                               '10',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: Colors.black,
-      //                                 fontSize: 12,
-      //                                 fontWeight: FontWeight.w600,
-      //                                 height: 0,
-      //                                 letterSpacing: 1.20,
-      //                               ),
-      //                             ),
-      //                             Text(
-      //                               'Donations',
-      //                               style: GoogleFonts.manrope(
-      //                                 color: const Color(0xFF20411B),
-      //                                 fontSize: 10,
-      //                                 fontWeight: FontWeight.w300,
-      //                                 height: 0,
-      //                                 letterSpacing: 1,
-      //                               ),
-      //                             ),
-      //                           ],
-      //                         )
-      //                       ],
-      //                     )
-      //                   ],
-      //                 ),
-      //               )
-      //             ],
-      //           ),
-      //           Column(
-      //             children: [
-      //               Container(
-      //                 width: 350,
-      //                 height: 1,
-      //                 margin: const EdgeInsets.all(10),
-      //                 decoration:
-      //                     const BoxDecoration(color: Color(0x541E1E1E)),
-      //               ),
-      //               Container(
-      //                 width: 340,
-      //                 height: 200,
-      //                 decoration: ShapeDecoration(
-      //                   image: DecorationImage(
-      //                     image: AssetImage(
-      //                         "backend/images/${story.bannerPicture}"),
-      //                     fit: BoxFit.cover,
-      //                   ),
-      //                   shape: RoundedRectangleBorder(
-      //                     borderRadius: BorderRadius.circular(10),
-      //                   ),
-      //                 ),
-      //               ),
-      //               Padding(
-      //                 padding: const EdgeInsets.all(9.0),
-      //                 child: Text(
-      //                   story.body,
-      //                   textAlign: TextAlign.justify,
-      //                 ),
-      //               ),
-      //               Row(
-      //                 crossAxisAlignment: CrossAxisAlignment.center,
-      //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //                 children: [
-      //                   Row(
-      //                     children: [
-      //                       const IconButton(
-      //                         onPressed: null,
-      //                         icon: Icon(Icons.thumb_up),
-      //                       ),
-      //                       Text(
-      //                         'XXKM',
-      //                         style: GoogleFonts.manrope(
-      //                           color: const Color(0xFF23401E),
-      //                           fontSize: 10,
-      //                           fontWeight: FontWeight.w700,
-      //                           height: 0,
-      //                           letterSpacing: 0.25,
-      //                         ),
-      //                       ),
-      //                     ],
-      //                   ),
-      //                   Row(
-      //                     children: [
-      //                       const IconButton(
-      //                         onPressed: null,
-      //                         icon: Icon(Icons.share),
-      //                       ),
-      //                       Text(
-      //                         'Share',
-      //                         style: GoogleFonts.manrope(
-      //                           color: const Color(0xFF23401E),
-      //                           fontSize: 10,
-      //                           fontWeight: FontWeight.w700,
-      //                           height: 0,
-      //                           letterSpacing: 0.25,
-      //                         ),
-      //                       ),
-      //                     ],
-      //                   ),
-      //                   Row(
-      //                     children: [
-      //                       const IconButton(
-      //                         onPressed: null,
-      //                         icon: Icon(Icons.edit),
-      //                       ),
-      //                       Text(
-      //                         'Edit',
-      //                         style: GoogleFonts.manrope(
-      //                           color: const Color(0xFF23401E),
-      //                           fontSize: 10,
-      //                           fontWeight: FontWeight.w700,
-      //                           height: 0,
-      //                           letterSpacing: 0.25,
-      //                         ),
-      //                       ),
-      //                     ],
-      //                   ),
-      //                 ],
-      //               ),
-      //             ],
-      //           )
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // )
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -328,7 +112,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     decoration: ShapeDecoration(
                       image: DecorationImage(
                         image:
-                            AssetImage("backend/images/${user.profilePicture}"),
+                            AssetImage("backend/images/${story.bannerPicture}"),
                         fit: BoxFit.cover,
                       ),
                       shape: RoundedRectangleBorder(
@@ -338,12 +122,12 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                 ),
                 // About
-                const Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 10.0),
                   child: Text(
-                    "Lorem ipsum dolor sit amet consectetur. Leo id a malesuada eu dapibus dis pulvinar venenatis. Eget gravida augue mauris ullamcorper nisi. Eget habitant odio lorem imperdiet aliquet ultrices faucibus. Sit dictum pellentesque mauris tempor. Ac facilisis tempor nunc arcu. Lectus vel semper suspendisse pellentesque bibendum in lacus arcu enim. Netus suspendisse mattis rutrum gravida amet quam. Ac ac nunc arcu cursus orci tempus. Sodales urna eleifend in ornare diam. Senectus eu nibh imperdiet nunc enim vitae nulla ac non. Cum est laoreet integer lorem. sit. Aliquet nisl vestibulum sit porta consectetur. Est nisl metus habitant aliquam consequat porta lectus posuere iaculis.",
-                    style: TextStyle(fontSize: 16.0),
+                    story.body,
+                    style: const TextStyle(fontSize: 16.0),
                     textAlign: TextAlign.justify,
                   ),
                 ),
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   static String uri = 'http://192.168.150.1:5000';
   static String homePCUri = 'http://192.168.19.87:5000';
-  static String ashishPCUri = 'http://192.168.6.171:5000';
+  static String ashishPCUri = 'http://192.168.11.59:5000';
 }
